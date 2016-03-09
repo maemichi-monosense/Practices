97bb109a2abc3a1c9a657dc1b078154644ccae81
yuya-m-ms <maemichi@monosense.co.jp>
2016-03-09 15:38:23 +0900

    Remove phpinfo

--- JS/Intro/Practice4.js
+++ JS/Intro/Practice4.js
@@ -2,14 +2,6 @@
 var VARS = new Object();
 VARS['host'] = 'http://192.168.33.15';
 
-// for debug use
-function phpinfo(id) {
-    $.get(VARS['host'] + "/phpinfo.php")
-        .done(function(html) {
-            $('#' + id).html(html);
-        });
-}
-
 function req_datetime(id) {
     $.get(VARS['host'] + "/datetime.php")
         .done(function(date) {
